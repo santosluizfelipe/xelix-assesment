67bc9a3aa887cb36a69fe921428204f3272fb85f
Luiz Santos <luizsantos@Luizs-MacBook-Air-2.local>
2024-06-04 16:56:56 +0100

    fix: rendered the table

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -6,13 +6,22 @@ import { TableInvoice } from "./Table.style";
 const Table = () => {
   return (
     <CardWrapper>
-      {data.pay_run.invoices.map((x) => {
+      {data.pay_run.invoices.map((invoice) => {
         return (
           <TableInvoice>
             <tbody>
+              <td>supplier's name</td>
+              <td>ammount</td>
+              <td>status</td>
+              <td>Posted date</td>
+              <td>due date</td>
               <tr>
-                <td>{x.supplier}</td>
-                <td>{x.amount}</td>
+                <td>{invoice.supplier}</td>
+                <td>{invoice.amount}</td>
+                <td>{invoice.status}</td>
+                <td>{invoice.posted_date}</td>
+                <td>{invoice.due_date}</td>
+                
               </tr>
             </tbody>
           </TableInvoice>
